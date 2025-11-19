4085264fe91be7d24e6323d0cbbbfc93c59529c5
VishalBhagour <vishalbhagour7@gmail.com>
2025-11-19 14:46:17 +0530

    [Vishal Bhagour] : Q-1

---
+++ Q-1.js
@@ -0,0 +1,13 @@
+let userName = "Manjeet";
+let currentHour = new Date().getHours();
+let greetingMessage = "";
+
+if (currentHour < 12) {
+    greetingMessage = `Good Morning ${userName}!`;
+} else if (currentHour >= 12 && currentHour < 17) {
+    greetingMessage = `Good Afternoon ${userName}!`;
+} else {
+    greetingMessage = `Good Evening ${userName}!`;
+}
+
+console.log(greetingMessage);
