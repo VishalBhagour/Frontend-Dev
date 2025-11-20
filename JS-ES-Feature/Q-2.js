0112d8235fc96c2241a456bb2fc22024a80005d1
VishalBhagour <vishalbhagour7@gmail.com>
2025-11-20 11:31:08 +0530

    [Vishal Bhagour] : Q-2

---
+++ JS-ES-Feature/Q-2.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const employees = [
+    { name: "Amit", salary: "45000", years: "5" },
+    { name: "Sara", salary: "38000", years: "2" },
+    { name: "Kiran", salary: "52000", years: "7" }
+];
+
+for (let emp of employees) {
+    try {
+        if (!emp.name || !emp.salary || !emp.years) throw "MissingProperty";
+
+        let salary = Number(emp.salary);
+        let years = Number(emp.years);
+
+        if (isNaN(salary) || isNaN(years)) throw "ConversionError";
+
+        let bonus = years > 3 ? salary * 0.1 : salary * 0.05;
+
+        console.log(`${emp.name} | Salary: ${salary} | Years: ${years} | Bonus: ${bonus}`);
+    } catch (err) {
+        console.log(`Error for employee ${emp.name}: ${err}`);
+    }
+}
